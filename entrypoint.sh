2589c77e82f7b96ce62de7e012bfcc857d0128b6
jaspercardol <37154053+jaspercardol@users.noreply.github.com>
2020-11-05 19:49:58 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,29 @@
-# /entrypoint.sh
 #!/bin/bash
 
-# Install something!
+
+# Update repo's!
+echo "http://dl-cdn.alpinelinux.org/alpine/edge/community" > /etc/apk/repositories
+echo "http://dl-cdn.alpinelinux.org/alpine/edge/main" >> /etc/apk/repositories
+
+# Update APK
+apk update
+
+# Install git
+apk add git
+
+# Install Chromium + Webdriver
+apk add chromium
+apk add chromium-chromedriver
+
+# Install Python and selenium
+apk add python3
+pip3 install -U selenium
+pip3 install -U python-pushover
+
+# Download MarktplaatsScraper
+git clone https://github.com/jaspercardol/MarktplaatsScraper.git
+cp MarktplaatsScraper /MarktplaatsScraper
+
+
+# Execute it.
 exec /init
